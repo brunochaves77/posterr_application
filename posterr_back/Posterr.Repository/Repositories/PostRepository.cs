b29df97b84180b2beb695c8fcea25c6501694efc
brunochaves77 <bruno-mg300@hotmail.com>
2024-06-25 10:21:15 -0300

    send project to github

---
+++ posterr_back/Posterr.Repository/Repositories/PostRepository.cs
@@ -0,0 +1,21 @@
+﻿using Posterr.Domain.Entities;
+using Posterr.Domain.Interfaces;
+
+namespace Posterr.Repository.Repositories {
+    public class PostRepository : RepositoryBase<Post>, IRepositoryBase<Post> {
+
+        public PostRepository(AppDataContext context) : base(context) { }
+
+        public int CountPostsByUsersInDay(Guid userId, DateTime date) {
+
+            var count = this.Context
+                .Posts
+                .Where(x => x.UserID == userId && x.CreatedAt.Date == date.Date)
+                .Count();
+
+            return count;
+
+        }
+
+    }
+}
